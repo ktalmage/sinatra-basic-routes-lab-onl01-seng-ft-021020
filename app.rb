c62ee7df420e677d5751335cea2b084aa8849a73
Kyle Talmage <ktalmage85@gmail.com>
2020-03-20 22:30:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+
+    "My name is Kyle"
+    end
+
+    get '/hometown' do
+
+    "My hometown is Calverton, NY"
+    end
+
+    get '/favorite-song' do
+
+    "My favorite song is Carry Me Ohio"
+    end
 end
